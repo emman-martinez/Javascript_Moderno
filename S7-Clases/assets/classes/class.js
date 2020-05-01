24a9bc61ae1436f644d0a654bd8b3602fcea2aea
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-04-30 19:15:05 -0500

    Setters y Getters, Propiedades estáticas

--- S7-Clases/assets/classes/class.js
+++ S7-Clases/assets/classes/class.js
@@ -6,9 +6,20 @@ console.log('');
 
 class Persona {
 
+    static _conteo = 0;
+    static get get_Conteo() {
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje() {
+        console.log(this.nombre); // undefined
+        console.log('Hola a todos, soy un método estático');
+    }
+
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
         
@@ -16,6 +27,16 @@ class Persona {
         this.codigo = codigo;
         this.frase = frase;
 
+        Persona._conteo++;
+
+    }
+
+    set setComida(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComida() {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
     }
 
     quienSoy() {
@@ -31,10 +52,22 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Yeah!');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman!');
-console.log({spiderman, ironman});
+// console.log({spiderman, ironman});
 
-//  spiderman.quienSoy();
 spiderman.miFrase();
-//  ironman.quienSoy();
-ironman.miFrase();
+//ironman.miFrase();
 
+/* **** => SETS y GETS <= **** */
+spiderman.setComida = 'El pie de cereza de la tía May';
+// console.log(spiderman.getComida);
+// console.log(spiderman);
+
+/* **** => PROPIEDADES ESTÁTICAS <= **** */ // Son Métodos y objetos que se pueden usar sin necesidad de instanciarlos
+// Persona._conteo = 2;
+console.log('Conteo estático: ',Persona._conteo); // Forma de saber cuantas instancias se han hecho de la clase
+console.log(Persona.get_Conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+console.log(Persona);
+console.log(Persona.propiedadExterna);
