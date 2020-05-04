195c2b792672f9f1195c1f4e7311122e3ea81cec
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-05-04 09:02:44 -0500

    Webpack Dev Server, Importando estilos de forma dinámica, Estilos Globales

--- S8-Modulos_Webpack/webpack.config.js
+++ S8-Modulos_Webpack/webpack.config.js
@@ -1,23 +1,48 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 
     mode: 'development',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
     module: {
         rules: [{
-            test: /\.html$/i,
-            loader: 'html-loader',
-            options: {
-                attributes: false,
-                minimize: false
+                test: /\.css$/i,
+                exclude: /styles\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
-        }]
+            {
+                test: /styles\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+                },
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html'
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
     ]
 
 }
