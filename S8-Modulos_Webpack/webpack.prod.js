59e1c2e4459f93c54e277e9e64a76192eb232c63
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-05-04 12:05:41 -0500

    Manejo de Imágenes, Webpack-Production Mode, Instalación y Configuración de Babel y Limpiando carpeta dist

--- S8-Modulos_Webpack/webpack.prod.js
+++ S8-Modulos_Webpack/webpack.prod.js
@@ -2,16 +2,28 @@ const HtmlWebPackPlugin       = require('html-webpack-plugin');
 const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin              = require('copy-webpack-plugin');
+const MinifyPlugin            = require('babel-minify-webpack-plugin');
 const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 
 module.exports = {
 
-    mode: 'development',
+    mode: 'production',
     optimization: {
         minimizer: [new OptimizeCssAssetsPlugin()]
     },
+    output: {
+        filename: "main.[contentHash].js"
+    },
     module: {
-        rules: [{
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
                 test: /\.css$/i,
                 exclude: /styles\.css$/i,
                 use: [
@@ -53,12 +65,13 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].[contentHash].css',
             ignoreOrder: false
         }),
         new CopyPlugin([
             {from: 'src/assets', to: 'assets/'}
         ]),
+        new MinifyPlugin(),
         new CleanWebpackPlugin()
     ]
 
